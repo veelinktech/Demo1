ab262bf06ebaae01ac83f06657bfe56b885d1029
veelinktech <104140334+veelinktech@users.noreply.github.com>
2023-07-03 16:45:51 +0530

    First commit
    Create sample.py program

---
+++ Sample.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+
+def setup_module(module):
+    global driver
+
+    s = Service(executable_path="D:\\SeleniumDrivers\\chromedriver_win32_latest\\chromedriver.exe")
+    driver = webdriver.Chrome(service=s)
+    driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+    driver.maximize_window()
+    driver.implicitly_wait(30)
+
+
+def teardown_module(module):
+    driver.close()
+
+
+def test_verifyTitle():
+    actual_title = driver.title
+    assert actual_title == "OrangeHRM"
+
+
